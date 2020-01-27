4e24921336df95d3a317da97c34874a31bb0f222
EfranH25 <ehimel@umass.edu>
2020-01-27 13:07:40 -0500

    test

--- Python Challanges/Challenge List Comprehension.py
+++ Python Challanges/Challenge List Comprehension.py
@@ -1,1 +1,12 @@
-print('hello')
+if __name__ == '__main__':
+
+    """
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    """
+
+    matrix = [[[j for j in range(1)] for i in range(1)] for k in range(1)]
+    print(matrix)
+    print(len(matrix))

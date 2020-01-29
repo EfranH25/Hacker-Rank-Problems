cffc7e472e5be18d49f8412cdf572048b45fdb8e
EfranH25 <ehimel@umass.edu>
2020-01-29 12:46:33 -0500

    test

--- Python Challanges/Challenge List Comprehension.py
+++ Python Challanges/Challenge List Comprehension.py
@@ -1,12 +1,16 @@
+def math(x, y, z, n):
+    matrix = []
+
+    [[[matrix.append([i, j, k]) for k in range(z+1) if i + j + k != n] for j in range(y+1)] for i in range(x+1)]
+    return matrix
+
+
 if __name__ == '__main__':
+    x = 1
+    y = 1
+    z = 1
+    n = 2
 
-    """
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
-    """
+    print(math(x,y,z,n))
 
-    matrix = [[[j for j in range(1)] for i in range(1)] for k in range(1)]
-    print(matrix)
-    print(len(matrix))
+
